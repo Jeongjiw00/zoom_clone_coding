745e9e462e3983e9b3c680495386f390cd02e1fb
Jeongjiw00 <wldn0229@naver.com>
2023-01-13 00:26:14 +0900

    add:ws-event&message

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,34 @@
-alert("hi");
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+//front에서 back으로 연결할때
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+//소켓이 연결되었으면 프론트의 console에
+socket.addEventListener("open", () => {
+  console.log("Connected to Server ✅");
+});
+// 소켓에 메세지가 들어오면
+socket.addEventListener("message", (message) => {
+  console.log("New message:", message.data);
+});
+// 연결이 끊겼을때
+socket.addEventListener("close", () => {
+  console.log("Disonnected from Server ❌");
+});
+
+// // 서버에 메세지 보내주기
+// setTimeout(() => {
+//   socket.send("hello from the browser");
+// }, 10000);
+
+//프론트엔드에서 백엔드 그러니까 서버로 값을 보내주기
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  socket.send(input.value);
+  // 보내줬으니까 비워주기
+  input.value = "";
+}
+
+messageForm.addEventListener("submit", handleSubmit);
--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import WebSocket from "ws";
 
 const app = express();
 
@@ -9,7 +11,38 @@ app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 //home.pug를 render
 app.get("/", (req, res) => res.render("home"));
+//페이지 홈만 쓸거라서
+app.get("/*", (req, res) => res.redirect("/"));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
-app.listen(3000, handleListen);
+// 서버 생성(http)
+const server = http.createServer(app);
+// websocket 서버 생성(http서버를 이용한) => http/ws 둘 다 사용가능
+// websocket.server는 객체를 전달인자로 받아주는데
+// {server, port}에서 포트를 생략해주면 http와 같은 port를 사용함!
+const wss = new WebSocket.Server({ server });
+
+function onSocketClose() {
+  console.log("Disonnected from the Browser ❌");
+}
+
+// function onSocketMessage(message) {
+//   //스트링으로 변환 안해주면 이상한 코드가 나옴
+//   console.log(message.toString("utf8"));
+// }
+
+// websocket작동
+// wss는 서버전체, socket은 브라우저 하나대상
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  //브라우저가 닫혔을때
+  socket.on("close", onSocketClose);
+  // 브라우저가 서버에 메세지 보냈을때 다시 그 값을 브라우저에 보내주기
+  socket.on("message", (message) => {
+    const messageString = message.toString("utf8");
+    socket.send(messageString);
+  });
+});
+
+server.listen(3000, handleListen);
